d0c3e7b4ae7d7c676ee13ed2bea86df00e88432e
Geovanny Alcon <151603150+g4usSi@users.noreply.github.com>
2025-09-04 10:51:30 -0600

    clase principal creada

---
+++ main.py
@@ -0,0 +1,10 @@
+class RegEx:
+    def __init__(self, pattern: str):
+        self.pattern = pattern
+
+    def match(self, string):
+        return string == self.pattern
+    
+    def patern(self):
+        return self.pattern
+    
